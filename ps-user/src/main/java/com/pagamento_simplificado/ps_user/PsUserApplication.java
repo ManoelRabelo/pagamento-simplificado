1ec255e733751d7a043addfb6b854376a9edd184
Manoel <manoelrabelo7@gmail.com>
2025-06-25 14:42:03 -0300

    feat: Project ps-user created

---
+++ ps-user/src/main/java/com/pagamento_simplificado/ps_user/PsUserApplication.java
@@ -0,0 +1,13 @@
+package com.pagamento_simplificado.ps_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsUserApplication.class, args);
+	}
+
+}
---
+++ ps-user/src/test/java/com/pagamento_simplificado/ps_user/PsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagamento_simplificado.ps_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
